d417b8b21823c6d2758d35d282e52b0570c4d6fa
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-05-15 23:19:00 +0100

    Comment Migration Worked on

--- database/migrations/2022_05_15_195456_create_comments_table.php
+++ database/migrations/2022_05_15_195456_create_comments_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\Movies;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +17,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Movies::class, 'movie_id');
+            $table->string('name', 1000);
+            $table->longText('comment');
             $table->timestamps();
         });
     }
